30d80c4f159bd54e171c495129eec0bdc316b80b
Emil Forsén <emilxforsen@gmail.com>
2024-09-25 15:20:39 +0200

    Added scenehandler

---
+++ Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    void OnEnable()
+    {
+        
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
