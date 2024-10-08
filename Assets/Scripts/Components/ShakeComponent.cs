5d6eab3b8f73cc81ab3dc4d265c23f5e69de61b9
Emil Forsén <emilxforsen@gmail.com>
2024-10-08 09:36:40 +0200

    Added DOTween

---
+++ Assets/Scripts/Components/ShakeComponent.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ShakeComponent : MonoBehaviour
+{
+    [SerializeField] bool shakeOnDamage;
+    [SerializeField] float shakeTime = 0;
+    [SerializeField] float shakeStrength = 0;
+    private Vector3 startPos;
+
+    void OnEnable()
+    {
+//        if (shakeOnDamage)
+        {
+            EventManager.OnTakeDamage += OnTakeDamage;
+        }
+    }
+
+    void OnDisable()
+    {
+        //if (shakeOnDamage)
+        {
+            EventManager.OnTakeDamage -= OnTakeDamage;
+        }
+    }
+
+    void Start()
+    {
+        startPos = GetComponent<Transform>().transform.position;
+    }
+
+    void OnTakeDamage(GameObject gameObject, int damage)
+    {
+        Shake();
+    }
+    void Shake()
+    {
+        transform.DOShakePosition(shakeTime, shakeStrength).OnComplete(() => transform.position = startPos);       
+    }
+
+}
